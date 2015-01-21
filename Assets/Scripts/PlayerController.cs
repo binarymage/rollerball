3ca008c0233ea8b80ef11759cad9fcd0b835e648
George Shore <george@georgeshore.com>
2015-01-21 16:06:17 +0000

    Displaying text and #WINNING
    Signed-off-by: George Shore <george@georgeshore.com>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    private int count;
+    public GUIText countText;
+    public GUIText winText;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
 
     void FixedUpdate()
     {
@@ -20,6 +30,17 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You WIN! YAY! A WINNER IS YOU!!";
         }
     }
 }
